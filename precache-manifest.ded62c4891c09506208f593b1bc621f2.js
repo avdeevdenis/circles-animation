3fc1be57acb59b2bedc2af9a556dc53a6e5b1aa2
Avdeev Denis <av-denis@yandex-team.ru>
2019-09-16 23:36:31 +0300

    Updates

---
+++ precache-manifest.ded62c4891c09506208f593b1bc621f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "759b10384fb5047a5fa6",
+    "url": "/circles-animation/static/js/runtime~main.759b1038.js"
+  },
+  {
+    "revision": "c3b14fe37ed6aa8a7bc4",
+    "url": "/circles-animation/static/js/main.c3b14fe3.chunk.js"
+  },
+  {
+    "revision": "0141f0108dbbd175501d",
+    "url": "/circles-animation/static/js/1.0141f010.chunk.js"
+  },
+  {
+    "revision": "c3b14fe37ed6aa8a7bc4",
+    "url": "/circles-animation/static/css/main.ce982b5b.chunk.css"
+  },
+  {
+    "revision": "567962fb01a56af787df1fbea6e751e0",
+    "url": "/circles-animation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/circles-animation/precache-manifest.8b34ff70a8370ce3ebc52a2181c5a79b.js"
+  "/circles-animation/precache-manifest.ded62c4891c09506208f593b1bc621f2.js"
 );
 
 workbox.clientsClaim();
