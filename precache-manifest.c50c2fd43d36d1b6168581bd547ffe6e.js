b2ec5e4171a1f4428f63f180c0738b218684e32d
Avdeev Denis <av-denis@yandex-team.ru>
2019-09-16 10:18:51 +0300

    Updates

---
+++ precache-manifest.c50c2fd43d36d1b6168581bd547ffe6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "759b10384fb5047a5fa6",
+    "url": "/circles-animation/static/js/runtime~main.759b1038.js"
+  },
+  {
+    "revision": "3dd4348ad503c35d867f",
+    "url": "/circles-animation/static/js/main.3dd4348a.chunk.js"
+  },
+  {
+    "revision": "869cc1ca16b538ac2f8e",
+    "url": "/circles-animation/static/js/1.869cc1ca.chunk.js"
+  },
+  {
+    "revision": "3dd4348ad503c35d867f",
+    "url": "/circles-animation/static/css/main.80ce8f6a.chunk.css"
+  },
+  {
+    "revision": "98bbea5327db1544e2000e8c17196ffc",
+    "url": "/circles-animation/index.html"
+  }
+];
